39193d9dab8f6dea576fe85e0460a609c0c89edd
Robert Malai <robert.malai@3pillarglobal.com>
2017-12-19 10:36:56 +0200

    Migrate the build scripts to amazonlinux 2 LTS base image.

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,18 @@
-FROM amazonlinux:2017.09
-
-ENV PS1='\u@\h [\w] \\$ '
-
-## Install build tools
-RUN yum group install -y 'Development tools'
+FROM amazonlinux:2
 
 ## Install python
-RUN yum install -y python36 && ln -s /usr/bin/pip-3.6 /usr/bin/pip
-
-## Install aws cli
-RUN pip install awscli && mkdir ~/.aws
+RUN amazon-linux-extras install python3 && \
+    pip-3.6 install --user awscli boto3 && mkdir ~/.aws
 
 # Install terraform
 RUN curl https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_linux_amd64.zip > terraform.zip \
-    && yum install -y unzip && unzip terraform.zip -d /opt && rm -f terraform.zip \
+    && yum install -y zip unzip && unzip terraform.zip -d /opt && rm -f terraform.zip \
     && chmod a+x /opt/terraform
 
-ENV PATH="${PATH}:/opt/"
+COPY build-lambda.sh /opt/build-lambda
+
+ENV PATH="${PATH}:/opt/" \
+    PS1="\u@\h [\w] \\$ "
 
 RUN mkdir /workspace
 
--- build-lambda.sh
+++ build-lambda.sh
@@ -7,25 +7,19 @@ if [ ! "$1" ]; then
     exit 1
 fi
 
-if [ ! -d "$1" ]; then
+FUNCTION_NAME=${1%}
+if [ ! -d "/workspace/$1" ]; then
     echo "Nu such function."
     exit 1
 fi
 
-FUNCTION_NAME=${1%/}
-VIRTUAL_ENVIRONMENT=.venv
-
-cd ${FUNCTION_NAME}
-
+VIRTUAL_ENVIRONMENT=/workspace/${FUNCTION_NAME}/.venv
 # Enable virtual environment
 if [ ! -d "$VIRTUAL_ENVIRONMENT" ]
 then
     echo "Setting up virtual environment"
     python3 -m venv ${VIRTUAL_ENVIRONMENT}
     source ${VIRTUAL_ENVIRONMENT}/bin/activate
-    pip install --upgrade pip
-    pip install --upgrade setuptools
-    pip install -r requirements.txt
     # The following dependencies are satisfied by AWS already, we keep them here
     # in order to support our development
     pip install boto3
@@ -35,10 +29,12 @@ fi
 
 echo "Installing dependencies..."
 # Bring dependencies from requirements.txt
-pip install -r requirements.txt -t out/${FUNCTION_NAME} --upgrade > /dev/null 2>&1
-# Copy the sources there
-cp -R *.py out/${FUNCTION_NAME} > /dev/null 2>&1
+pip install -r /workspace/${FUNCTION_NAME}/requirements.txt -t /tmp/${FUNCTION_NAME} --upgrade > /dev/null 2>&1
 
 echo "Create zip bundle..."
-# Pack the bundle
-cd out/${FUNCTION_NAME} && zip -r9 ../${FUNCTION_NAME}.zip * > /dev/null 2>&1
+mkdir -p /workspace/${FUNCTION_NAME}/out && \
+    cd /tmp/${FUNCTION_NAME} > /dev/null && \
+    zip -r9 /workspace/${FUNCTION_NAME}/out/${FUNCTION_NAME}.zip * > /dev/null 2>&1 && \
+    cd /workspace/${FUNCTION_NAME} > /dev/null && \
+    zip -9 /workspace/${FUNCTION_NAME}/out/${FUNCTION_NAME}.zip *.py > /dev/null 2>&1 && \
+    cd - > /dev/null && rm -R /tmp/${FUNCTION_NAME}
